1c942d62bca9fd07d4e5b9b351fa683eb9a3d184
Brian Helms <bhelms2132@gmail.com>
2022-08-05 21:30:37 -0400

    Primitive data type examples added to js file

---
+++ JS_SANDBOX/Data_Types/app.js
@@ -0,0 +1,25 @@
+// PRIMITIVE DATA TYPES
+
+// String
+const name = 'John Doe';
+console.log(typeof name); // string
+
+// Number
+const age = 45;
+console.log(typeof age); // number
+
+// Boolean
+const hasKids= true;
+console.log(typeof hasKids); // boolean
+
+// Null
+const car = null;
+console.log(typeof car); // object
+
+// Undefined
+let test;
+console.log(typeof test); // undefined
+
+// Symbol
+const sym = Symbol();
+console.log(typeof sym); // symbol
