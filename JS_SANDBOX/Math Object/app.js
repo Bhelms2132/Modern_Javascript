15d153c7371dd6643e52f6d7bc12da7d037c2f4b
Brian Helms <bhelms2132@gmail.com>
2022-08-08 19:39:19 -0400

    Math object examples added to js file

--- JS_SANDBOX/Math Object/app.js
+++ JS_SANDBOX/Math Object/app.js
@@ -9,6 +9,21 @@ val = num1 / num2; // Dived
 val = num1 % num2; // Modulus
 
 // Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2,1,3,4,77,33,22);
+val = Math.max(2,1,3,4,77,33,22);
+val = Math.random(); // Will return random decimal number
+
+val = Math.floor(Math.random() * 20 + 1); // Must wrap .random() with .floor() to return a whole number
+
+
 
 
 
