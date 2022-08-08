46b0c9a70bc91fa8bf7732e7c33b3617afb59116
Brian Helms <bhelms2132@gmail.com>
2022-08-08 19:17:40 -0400

    Simple math equations added to js file

--- JS_SANDBOX/Math Object/app.js
+++ JS_SANDBOX/Math Object/app.js
@@ -2,6 +2,15 @@ const num1 = 100;
 const num2 = 50;
 
 // Simple math with numbers
-val = num1 + num2;
+val = num1 + num2; // Add 
+val = num1 * num2; // Multiply
+val = num1 - num2; // Subtract
+val = num1 / num2; // Dived
+val = num1 % num2; // Modulus
+
+// Math Object
+
+
+
 
 console.log(val);
