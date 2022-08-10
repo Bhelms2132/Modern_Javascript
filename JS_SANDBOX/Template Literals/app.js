e95ddc35734c942acd297d07d47b800998ae0400
Brian Helms <bhelms2132@gmail.com>
2022-08-09 20:21:53 -0400

    Template Literal/Template String created using the same const variables

--- JS_SANDBOX/Template Literals/app.js
+++ JS_SANDBOX/Template Literals/app.js
@@ -4,8 +4,28 @@ const age = 45;
 const job = 'Web Developer';
 const city = 'Concord';
 
-// HTML string, without template strings
+// HTML string, without template strings (es5)
 html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>'; 
+
+html = '<ul>' +
+        '<li>Name: ' + name + '</li>' +
+        '<li>Age: ' + age + '</li>' +
+        '<li>Job: ' + job + '</li>' +
+        '<li>City: ' + city + '</li>' +
+        '</ul>'; 
+
+
+
+
+// With template literals (es6)
+html = `
+  <ul>
+  <li>Name: ${name}</li>
+  <li>Age: ${age}</li>
+  <li>Job: ${job}</li>
+  <li>City: ${city}</li>
+  </ul>`;
+
 
 // Renders to the 'DOM' Document Object Model
 document.body.innerHTML = html;
