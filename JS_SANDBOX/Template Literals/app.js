f5a78e20fb26033d44b2a0b7a0e5b70617efaf02
Brian Helms <bhelms2132@gmail.com>
2022-08-09 20:31:39 -0400

    More examples of what can be excuted using template literals added to js file

--- JS_SANDBOX/Template Literals/app.js
+++ JS_SANDBOX/Template Literals/app.js
@@ -15,15 +15,21 @@ html = '<ul>' +
         '</ul>'; 
 
 
-
+function hello() {
+    return 'hello';
+}
 
 // With template literals (es6)
 html = `
+  <h1>${'Template Literals'}</h1>
   <ul>
   <li>Name: ${name}</li>
   <li>Age: ${age}</li>
   <li>Job: ${job}</li>
   <li>City: ${city}</li>
+  <li>${2 + 2}</li>
+  <li>${hello()}</li>
+  <li>${age > 40 ? 'Over 40' : 'Under 40'}</li>
   </ul>`;
 
 
