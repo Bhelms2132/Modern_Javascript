b376e1ffdf00e0e042db119aab3f6914936a3f39
Brian Helms <bhelms2132@gmail.com>
2022-08-11 21:06:29 -0400

    Renamed folder due to & symbol creating issues and would not load page

---
+++ JS_SANDBOX/Arrays_Array Methods/app.js
@@ -0,0 +1,11 @@
+// Create some arrays
+const numbers = [57,84,3,40,29];
+const numbers2 = new Array(94,54,2,56,6);
+const fruit = ['Apple', 'Orange', 'Blueberry', 'Strawberry'];
+const mixed = [4, 'Hello', true, undefined, null, {a:2, b:2}, new Date()];
+
+let val;
+
+// Get Array
+
+console.log(numbers);
