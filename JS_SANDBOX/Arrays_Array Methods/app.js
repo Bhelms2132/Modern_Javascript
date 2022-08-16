07b14bfa4ad7f08cec408658418aa3459afacede
Brian Helms <bhelms2132@gmail.com>
2022-08-16 19:15:32 -0400

    Mutating arrays examples added to js file

--- JS_SANDBOX/Arrays_Array Methods/app.js
+++ JS_SANDBOX/Arrays_Array Methods/app.js
@@ -7,5 +7,29 @@ const mixed = [4, 'Hello', true, undefined, null, {a:2, b:2}, new Date()];
 let val;
 
 // Get Array
+val = numbers.length;
+// Check if is array
+val = Array.isArray(numbers);
+// Get single value
+val = numbers[1];
+// Insert into array
+numbers[5] = 99;
+//  Find index of value
+val = numbers.indexOf(29);
+
+//MUTATING ARRAYS
+// Add on to end
+numbers.push(21);
+// Add on to front
+numbers.unshift(44);
+// Take off from end
+numbers.pop();
+// Take off from front
+numbers.shift();
+// Splice values
+numbers.splice(1,2);
+// Reverse
+numbers.reverse();
 
 console.log(numbers);
+console.log(val);
