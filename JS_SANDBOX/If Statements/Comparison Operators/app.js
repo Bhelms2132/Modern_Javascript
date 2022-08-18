a513130bdfc3cda6b5ea5a5c7c1609999ef1514f
Brian Helms <bhelms2132@gmail.com>
2022-08-17 20:57:47 -0400

    Logical AND && expression used in an if else example

--- JS_SANDBOX/If Statements/Comparison Operators/app.js
+++ JS_SANDBOX/If Statements/Comparison Operators/app.js
@@ -49,4 +49,27 @@ const id = 100;
 //     console.log('False');
 // }
 
-// 
+// IF ELSE STATEMENT
+let color = 'Green';
+
+// if(color === 'Red'){
+//     console.log('Color is Red');
+// } else if(color === 'Blue') {
+//     console.log('Color is Blue');
+// } else {
+//     console.log('Color is not Red or Blue');
+// }
+
+// LOGICAL OPERATORS
+
+const name = 'Steve';
+const age = 2;
+
+// && represents logical AND 
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if(age >= 13 && age <= 20){
+    console.log(`${name} is under 21`);
+} else {
+    console.log(`${name} is over 21`);
+}
