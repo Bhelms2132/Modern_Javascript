a014385d514b27a2489c79f7be73e2c37f6e6102
Brian Helms <bhelms2132@gmail.com>
2022-08-17 20:18:28 -0400

    if else statements added to js file along with equal value ==, equal value and type ===, not equal value !=, not equal value and type !== examples

---
+++ JS_SANDBOX/If Statements/Comparison Operators/app.js
@@ -0,0 +1,38 @@
+// if(something){
+//     do something
+// } else {
+//     do something else
+
+// };
+
+const id = 100;
+
+// EQUAL TO VALUE
+if(id == 100){
+    console.log('Correct');
+} else {
+    console.log('Incorrect');
+}
+
+// NOT EQUAL TO VALUE
+// if(id != 100){
+//     console.log('Correct');
+// }else {
+//     console.log('Incorrect');
+// }
+
+// EQUAL TO VALUE & TYPE
+// if(id === 100){
+//     console.log('Correct');
+// } else {
+//     console.log('Incorrect');
+// }
+
+// NOT EQUAL TO VALUE & TYPE
+// if(id !== 100){
+//     console.log('Correct');
+// } else {
+//     console.log('Incorrect');
+// }
+
+
