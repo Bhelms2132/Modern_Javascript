31c7dea3afcc61ab148b4453893d3f740f3c2129
Brian Helms <bhelms2132@gmail.com>
2022-08-24 14:56:31 -0400

    function declaration created with two paramaters firstName and lastName

---
+++ JS_SANDBOX/Function Declarations/app.js
@@ -0,0 +1,11 @@
+// FUNCTION DECLARATIONS
+
+ function greet(firstName, lastName) {
+    // function scope
+    // console.log('Hello!');
+    return 'Hello ' + firstName + ' ' + lastName;
+ }
+
+ // call function
+//  greet();
+console.log(greet('Bob', 'Smith'));
