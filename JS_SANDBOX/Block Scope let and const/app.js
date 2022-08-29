01ec9b10d0b986567492642e76d6c6b4572494b1
Brian Helms <bhelms2132@gmail.com>
2022-08-29 17:58:33 -0400

    Dom Manipulation and Events folder created to examine and study how the dom works. This folder will have examples of how to manipulate the dom and work with events

--- JS_SANDBOX/Block Scope let and const/app.js
+++ JS_SANDBOX/Block Scope let and const/app.js
@@ -12,12 +12,16 @@ const c = 3;
 
 // test();
 
-if (true) {
+// if (true) {
     // Block Scope
-    var a = 4;
-    let b = 5;
-    const c = 6;
-    console.log('Block Scope: ', a, b, c);
-}
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('Block Scope: ', a, b, c);
+// }
 
-console.log('Global Scope: ', a, b, c);
+// for(let b = 0; b < 10; b++) {
+//     console.log(`Loop: ${b}`);
+// }
+
+// console.log('Global Scope: ', a, b, c);
