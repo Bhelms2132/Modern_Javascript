4495e8edc4d422601d8ea93f579afc8601bc656a
Brian Helms <bhelms2132@gmail.com>
2022-08-29 17:28:46 -0400

    Block Scope created with an if method

--- JS_SANDBOX/Block Scope let and const/app.js
+++ JS_SANDBOX/Block Scope let and const/app.js
@@ -3,13 +3,21 @@ var a = 1;
 let b = 2;
 const c = 3;
 
-function test() {
+// function test() {
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('Function Scope: ', a, b, c);
+// }
+
+// test();
+
+if (true) {
+    // Block Scope
     var a = 4;
     let b = 5;
     const c = 6;
-    console.log('Function Scope: ', a, b, c);
+    console.log('Block Scope: ', a, b, c);
 }
-
-test();
 
 console.log('Global Scope: ', a, b, c);
