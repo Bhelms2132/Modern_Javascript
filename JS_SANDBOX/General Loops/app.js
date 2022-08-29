0c5898a66e8715c150f1f7b70f20896f89fc236b
Brian Helms <bhelms2132@gmail.com>
2022-08-29 13:20:59 -0400

    while loop example added to app.js

--- JS_SANDBOX/General Loops/app.js
+++ JS_SANDBOX/General Loops/app.js
@@ -1,24 +1,35 @@
-// FOR LOOP
+// FOR LOOP - Use FOR LOOP when you know how many times you want the loop to run
 
-for(let i = 0; i < 10; i++){
-    if(i === 2){
-        console.log('2 is a rounded number');
-        continue;
-    }
-    if(i === 4){
-        console.log('4 is a rounded number');
-        continue;
-    }
-    if(i === 6){
-        console.log('6 is a rounded number');
-        continue;
-    }
-    if(i === 8){
-        console.log('8 is a rounded number');
-        continue;
-    }
-    if(i === 9){
-        break;
-    }
+// for(let i = 0; i < 10; i++){
+//     if(i === 2){
+//         console.log('2 is a rounded number');
+//         continue;
+//     }
+//     if(i === 4){
+//         console.log('4 is a rounded number');
+//         continue;
+//     }
+//     if(i === 6){
+//         console.log('6 is a rounded number');
+//         continue;
+//     }
+//     if(i === 8){
+//         console.log('8 is a rounded number');
+//         continue;
+//     }
+//     if(i === 9){
+//         break;
+//     }
+//     console.log('Number ' + i);
+// }
+
+
+// WHILE LOOP - Use the WHILE LOOP when you're not certain on how many times the loop should run
+
+// Set variable outside of the loop
+let i = 0;
+
+while (i < 10) {
     console.log('Number ' + i);
+    i++;
 }
