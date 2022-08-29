c6ff131c3494473375582ebdff7af382bf007a76
Brian Helms <bhelms2132@gmail.com>
2022-08-29 17:22:54 -0400

    variables created in both global scope and function scope

---
+++ JS_SANDBOX/Block Scope let and const/app.js
@@ -0,0 +1,15 @@
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+function test() {
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log('Function Scope: ', a, b, c);
+}
+
+test();
+
+console.log('Global Scope: ', a, b, c);
