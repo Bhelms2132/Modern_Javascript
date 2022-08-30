f1b163c6db1ad4988778a625e13b2fe006837fca
Brian Helms <bhelms2132@gmail.com>
2022-08-30 19:17:56 -0400

    Examples of how to log properties in the dom

---
+++ Dom Manipulation and Events/Examining the Document Object/app.js
@@ -0,0 +1,20 @@
+let val;
+
+// Log properties in the DOM
+val = document; // logs HTML document
+val = document.all; // Gives us a collection of all tags within an array
+val = document.all[9]; // Gives us a tag for the number within the array bracket
+val = document.all.length; // Logs how many elements are in the DOM
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+
+
+
+
+console.log(val);
