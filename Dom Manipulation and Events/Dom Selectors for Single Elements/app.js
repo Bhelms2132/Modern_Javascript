e96bb51b601a2650e5dacab15da6f56d241ad3b9
Brian Helms <bhelms2132@gmail.com>
2022-08-30 19:51:14 -0400

    Examples of how to target and manipulate id using getElementById() and changing styling added to app.js

---
+++ Dom Manipulation and Events/Dom Selectors for Single Elements/app.js
@@ -0,0 +1,9 @@
+// document.getElementById()
+
+console.log(document.getElementById('task-title'));
+
+// Get things from the element
+console.log(document.getElementById('task-title').id);
+
+// Change styling
+document.getElementById('task-title').style.background = '#333';
